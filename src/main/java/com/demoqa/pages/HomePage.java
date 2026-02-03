58e1ac115941ce0fdb95e8db3e57df87088d0d81
Ira Baker <paxpistor@gmail.com>
2026-02-03 13:48:52 +0100

    First commit

---
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -0,0 +1,29 @@
+package com.demoqa.pages;
+
+import com.demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".card:nth-child(6)")
+    WebElement bookStore;
+
+    public SidePanel getBookStore() {
+        click(bookStore);
+        return new SidePanel(driver);
+    }
+
+    @FindBy(css = ".card:nth-child(3)")
+    WebElement alertsFrameWindows;
+
+    public SidePanel getAlertsFrameWindows() {
+        clickWithJS(alertsFrameWindows,0,300);
+        return new SidePanel(driver);
+    }
+}
