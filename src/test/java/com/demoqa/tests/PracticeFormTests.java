0d1a1f1b8f288bc7ca9960d875409cd768deb497
Ira Baker <paxpistor@gmail.com>
2026-02-09 12:53:28 +0100

    Updated

--- src/test/java/com/demoqa/tests/PracticeFormTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -3,19 +3,49 @@ package com.demoqa.tests;
 import com.demoqa.core.TestBase;
 import com.demoqa.pages.HomePage;
 import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.forms.PracticeFormPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PracticeFormTests extends TestBase {
 
+    PracticeFormPage practiceForm;
+
     @BeforeEach
     public void precondition() {
         new HomePage(driver).getForms();
         new SidePanel(driver).getPracticeForm();
+        practiceForm = new PracticeFormPage(driver);
     }
 
     @Test
     public void createAccountPositiveTest() {
+        practiceForm.enterPersonalData("Jamal","Musiala","jamal@gm.com","1234567890")
+                .selectGender("Male")
+                .typeOfDate("16 Aug 1987")
+                .addSubject(new String[]{"Maths","English"})
+                .selectHobby(new String[]{"Sports","Music"})
+                .uploadFile("/Users/baker/Tools/1.png")    // C:/Tools/1.jpg
+                .enterState("NCR")
+                .enterCity("Delhi")
+                .submit()
+                .verifySuccessRegistration("Thanks for submitting the form")
+                ;
+    }
 
+    @Test
+    public void createAccountNegativeWithInvalidPhoneTest() {
+        practiceForm.enterPersonalData("Jamal","Musiala",
+                        "jamal@gm.com","1234")
+                .selectGender("Male")
+                .selectDate("August","1987","16")
+                .addSubject(new String[]{"Maths","English"})
+                .selectHobby(new String[]{"Sports","Music"})
+                .uploadFile("/Users/baker/Tools/1.png")    // C:/Tools/1.jpg
+                .enterState("NCR")
+                .enterCity("Delhi")
+                .submit()
+                .verifyFormTitle()
+                ;
     }
 }
